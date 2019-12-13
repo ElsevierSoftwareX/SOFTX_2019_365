1b815b9f9513a15d043805d23dbdb5bb44f47d21
Panagiotis Hadjidoukas <phadjido@gmail.com>
2019-12-14 00:44:52 +0100

    updated

---
+++ examples/example1.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 13:24:31 2019
+
+@author: nicka
+"""
+import sys
+import json
+from DiscreteLatticeMech import Solver, Writer
+
+if __name__ == "__main__":
+
+    if len(sys.argv) == 1:
+        print("Usage: {} <input filename (json)>".format(sys.argv[0]))
+        filepath = 'InputData_SquareEx.json'
+    else:
+        filepath = sys.argv[1]
+
+    try:
+        with open(filepath, 'r') as f:
+            data = json.load(f)
+    except IOError as error:
+        print("could not open input file {}".format(filepath))
+        sys.exit(1)
+
+    solver = Solver()
+    solver.solve(data)
+
+    writer = Writer()
+    writer.WriteTensorsToFile(solver.CMatTensor, solver.FlexMatTensor)
+    writer.WriteEffectivePropertiesToFile(solver.Bulk, solver.Ex, solver.Ey, solver.Poissonyx, solver.Poissonxy, solver.G, solver.rho)
+    writer.PlotEffectiveProperties(solver.Bulk, solver.Ex, solver.Ey, solver.Poissonyx, solver.Poissonxy, solver.G)
---
+++ examples/example2.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 13:24:31 2019
+
+@author: nicka
+"""
+import sys
+import json
+from DiscreteLatticeMech import Solver, Writer
+
+
+if __name__ == "__main__":
+
+    data = {
+        'Delta1': [1, 0],
+        'Delta2': [0, 1],
+        'Eb': [1, 1],
+        'Ka': [21, 21],
+        'Kb': [0.21, 0.21],
+        'L1': 10,
+        'L2': 10,
+        'Lb': [10, 10],
+        'NumberElements': 2,
+        'NumberNodes': 1,
+        'Ob': [1, 1],
+        'Y_1': [1, 0],
+        'Y_2': [0, 1],
+        'e_1': [1, 0],
+        'e_2': [0, 1],
+        'tb': [1, 1]}
+
+
+    solver = Solver()
+    solver.solve(data)
+
+    writer = Writer()
+    writer.WriteTensorsToFile(solver.CMatTensor, solver.FlexMatTensor)
+    writer.WriteEffectivePropertiesToFile(solver.Bulk, solver.Ex, solver.Ey, solver.Poissonyx, solver.Poissonxy, solver.G, solver.rho)
+    writer.PlotEffectiveProperties(solver.Bulk, solver.Ex, solver.Ey, solver.Poissonyx, solver.Poissonxy, solver.G)
