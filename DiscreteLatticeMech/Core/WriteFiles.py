50e005fbcf074e54b0de66f4de53d5d45663eb4a
nkarathan <57357853+nkarathan@users.noreply.github.com>
2019-12-16 14:14:49 +0100

    Update WriteFiles.py

--- DiscreteLatticeMech/Core/WriteFiles.py
+++ DiscreteLatticeMech/Core/WriteFiles.py
@@ -16,7 +16,7 @@ class Writer:
         os.mkdir(self.folder)
         print("Results folder: {}".format(self.folder))
 
-    def WriteTensorsToFile(self, CMatTensor, FlexMatTensor):
+    def WriteTensorsToFile(self, InputData, CMatTensor, FlexMatTensor):
         # Store the InputData in the Result file
         with open(self.folder + '/InputData.json', 'w',encoding='utf-8') as outfile:
            json.dump(InputData, outfile, ensure_ascii=False,  indent=6, sort_keys=False)
